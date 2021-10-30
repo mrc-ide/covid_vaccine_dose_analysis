77b2b1dfadb5364e0bb571e1f83d97e887a95478
abhogan <hogan.alexandra@gmail.com>
2021-10-30 22:52:47 +0930

    update impact plots to show single icg, include doses over time

--- 3_plots_impact_scenario1.R
+++ 3_plots_impact_scenario1.R
@@ -1,6 +1,11 @@
-df_summarise <- readRDS("processed_outputs/df_summarise_scenario1.rds") %>%
-  filter(!(strategy_name != "Counterfactual" & timestep < 300))
-df_summarise_totals <- readRDS("processed_outputs/df_summarise_totals_scenario1.rds")
+icg = "HIC"
+
+df_summarise <- readRDS("processed_outputs_old/df_summarise_scenario1.rds") %>%
+  filter(!(strategy_name != "Counterfactual" & timestep < 300)) %>%
+  filter(income_group == icg)
+
+df_summarise_totals <- readRDS("processed_outputs_old/df_summarise_totals_scenario1.rds") %>%
+  filter(income_group == icg)
 
 counterfactual <- df_summarise %>%
   filter(max_coverage == 0) %>%
@@ -10,7 +15,6 @@ counterfactual <- df_summarise %>%
 p1 <- ggplot(data = df_summarise, aes(x = as.Date(date), y = deaths_t/target_pop * 1e6, col = strategy_name)) +
   geom_ribbon(aes(ymin =deaths_tmin/target_pop * 1e6, ymax = deaths_tmax/target_pop * 1e6, fill = strategy_name), alpha = 0.5, col = NA) +
   geom_line() +
-  facet_wrap( ~ income_group) +
   theme_bw() +
   theme(strip.background = element_rect(fill = NA),
         panel.border = element_blank(),
@@ -18,14 +22,13 @@ p1 <- ggplot(data = df_summarise, aes(x = as.Date(date), y = deaths_t/target_pop
         legend.text.align = 0) +
   scale_color_manual(values = c("black", col6, col8, col1)) +
   scale_fill_manual(values = c("darkgrey", col6, col8, col1)) +
-  labs(x = "Timestep (days)", y = "Daily deaths per million", col = "Dose scenario", fill = "Dose scenario")
+  labs(x = "Year", y = "Daily deaths per million", col = "Dose scenario", fill = "Dose scenario")
 p1
-ggsave("plots/scenario1_impact_time.png", height = 4.5, width = 13)
+ggsave(paste0("plots/scenario1_impact_time_", icg, ".png"), height = 4.5, width = 10)
 
 # barplot summary of deaths
 p2 <- ggplot(data = df_summarise_totals, aes(x = strategy_name, y = deaths_med/target_pop * 1e6, fill = strategy_name)) +
   geom_bar(position = "dodge", stat = "identity") +
-  facet_wrap(~income_group) +
   scale_fill_manual(values = c("grey", col6, col8, col1)) +
   labs(x = "Dose scenario", y = "Total deaths per million since vaccination start", col = "Dose scenario", fill = "Dose scenario") +
   theme_bw() +
@@ -35,7 +38,7 @@ p2 <- ggplot(data = df_summarise_totals, aes(x = strategy_name, y = deaths_med/t
         legend.text.align = 0,
         axis.text.x = element_text(angle = 335, vjust = 0.5, hjust=0))
 p2 
-ggsave("plots/scenario1_summary.png", height = 4.5, width = 8)
+ggsave(paste0("plots/scenario1_summary_", icg, ".png"), height = 4.5, width = 5)
 
 # summary of deaths averted per dose
 df_summarise_averted <- df_summarise_totals
@@ -50,7 +53,6 @@ df_summarise_averted <- df_summarise_averted %>%
 
 p3 <- ggplot(data = df_summarise_averted, aes(x = strategy_name, y = deaths_averted/total_doses_med * 100, fill = strategy_name)) +
   geom_bar(position = "dodge", stat = "identity") +
-  facet_wrap(~income_group) +
   scale_fill_manual(values = c(col6, col8, col1)) +
   labs(x = "Dose scenario", y = "Total deaths averted per 100 doses", col = "Dose scenario", fill = "Dose scenario") +
   theme_bw() +
@@ -61,10 +63,30 @@ p3 <- ggplot(data = df_summarise_averted, aes(x = strategy_name, y = deaths_aver
         axis.text.x = element_text(angle = 335, vjust = 0.5, hjust=0))
 
 p3
-ggsave("plots/scenario1_summary_perdose.png", height = 4.5, width = 8)
+ggsave(paste0("plots/scenario1_summary_perdose", icg, ".png"), height = 4.5, width = 5)
 
 p2 <- p2 + theme(legend.position = "none")
 p3 <- p3 + theme(legend.position = "none")
 combined <- p1 / (p2 | p3) + plot_annotation(tag_levels = "A")+ plot_layout(guides = "collect")
 combined
 ggsave("plots/scenario1_summary.png", height = 8, width = 10)
+
+# plot vaccines delivered over time
+# show total doses over time as a check
+df2 <- filter(df_summarise, max_coverage == 0.8) 
+p0 <- ggplot(data = df2, aes(x = as.Date(date), y  = vaccines_t/target_pop, col = strategy_name)) +
+  geom_line(size = 1) +
+  lims(x = c(as.Date("2020-01-01"), as.Date("2023-01-01"))) +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = NA),
+        panel.border = element_blank(),
+        axis.line = element_line(),
+        legend.text.align = 0) +
+  labs(x = "Year", y = "Cumulative doses delivered per million", col = "Dose scenario") +
+scale_color_manual(values = c(col6, col8, col1))
+  
+p0
+
+combined <- p0 / p1 / (p2 | p3) + plot_annotation(tag_levels = "A")+ plot_layout(guides = "collect")
+combined
+ggsave(paste0("plots/scenario1_summary_", icg, ".png"), height = 8, width = 10)
